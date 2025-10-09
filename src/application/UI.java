df880d236c02ae8afbee4863609f728ba4e87d85
Vicente Alves Gregório Netto <09235200156@tocantins.governo>
2025-10-09 09:45:37 -0300

    starting to moving pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,10 @@ import chess.ChessPiece;
 import java.util.ArrayList;
 import chess.Color;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import chess.ChessPosition;
+
 public class UI {
 
     // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
@@ -25,6 +29,17 @@ public class UI {
     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        } catch (RuntimeException e) {
+            throw new InputMismatchException("error reading chess position");
+        }
+    }
 
     public static void printBoard(ChessPiece[][] pieces){
         for(int i=0;i<pieces.length;i++){
@@ -51,5 +66,6 @@ public class UI {
         }
         System.out.print(" ");
     }
+
 
 }
--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,7 +1,9 @@
 package chess;
+import boardgame.BoardException;
+
 import java.io.Serial;
 
-public class ChessException extends RuntimeException {
+public class ChessException extends BoardException {
     @Serial
     private static final long serialVersionUID= 1L;
     public ChessException(String msg) {
