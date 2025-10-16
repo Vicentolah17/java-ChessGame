f2b47e1cab131d4f7dc9506b7622769df5e11fab
Vicente Alves Gregório Netto <09235200156@tocantins.governo>
2025-10-16 11:55:18 -0300

    printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,9 +22,16 @@ public class Program {
                 System.out.println("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
 
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
                 System.out.println();
                 System.out.println("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
+
+                System.out.println();
+                System.out.println("Target: ");
+
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
             }
