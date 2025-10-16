9c1adc6ec3ed627a6d2600c8a94b8439a1d4f1ae
Vicente Alves Gregório Netto <09235200156@tocantins.governo>
2025-10-16 11:31:40 -0300

    implementing possible moves of a Rook

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -24,6 +24,11 @@ public class Position {
     public void setRow(int row) {
         this.row = row;
     }
+    public void setValue(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
     @Override
     public String toString() {
         return row + "," + column;
